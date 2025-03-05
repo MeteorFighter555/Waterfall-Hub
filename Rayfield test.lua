df1cb965fd1e48145d002fb75efbd52ba2b95c86
MeteorFighter555 <ginnarak@gmail.com>
2025-03-05 13:16:39 +0700

    Update Rayfield test.lua

--- Rayfield test.lua
+++ Rayfield test.lua
@@ -23,7 +23,7 @@ local Window = ArrayField:CreateWindow({
             FileName = "ArrayFieldsKeys",
             SaveKey = false,
             GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like ArrayField to get the key from
-            Key = {"U2FsdGVkX1+swGe5EB0AmgaTPqccOA7tc7cTnLJ2pEU="},
+            Key = {"DoMkEeHoMhOm",'MeteorXEntity'},
             Actions = {
                 [1] = {
                     Text = 'No Key link Provided loser',
