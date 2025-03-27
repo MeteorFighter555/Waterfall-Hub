4c8b9933c363f85f1edcc71ba2a3a3580b2d94d7
MeteorFighter555 <ginnarak@gmail.com>
2025-03-27 16:14:08 +0700

    Update Rayfield.lua

--- Rayfield.lua
+++ Rayfield.lua
@@ -56,6 +56,9 @@ local Button = HomeTab:CreateButton({
    Name = "Server Hop",
    Callback = function()
          
+         local module = loadstring(game:HttpGet"https://raw.githubusercontent.com/LeoKholYt/roblox/main/lk_serverhop.lua")()
+
+         module:Teleport(game.PlaceId)
          
    end,
 })
