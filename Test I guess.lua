7ea548bf37cf157c85a935034985ca8f0ab38068
MeteorFighter555 <ginnarak@gmail.com>
2025-04-07 09:55:44 +0700

    Create Test I guess.lua

---
+++ Test I guess.lua
@@ -0,0 +1,38 @@
+local Library = loadstring(game:HttpGet("comingsoon.lua"))()
+
+Library.load.CreateLoader({
+    Title = "Moonrock Hub",
+    Thumbnail = "35500340", -- This must be the ID of the image you want to be the thumbnail.
+    Discord = {
+        Invite = "rXbGpfuKmV" -- You do not need to have the "https://discord.gg/" part of the invite here, only the code.
+    }
+})
+
+Library:CreateWindow({
+    Title = "Moonrock │ discord.gg/rXbGpfuKmV │ Exploit with style",
+    Discord = {
+        Invite = "rXbGpfuKmV" -- You do not need to have the "https://discord.gg/" part of the invite here, only the code.
+    },
+    ConfigSaving = {
+        Enabled = true,
+        FolderTitle = "Moonrock",
+        FileTitle = "Moonrock Hub"
+    }
+})
+
+local ExampleTab = Library:CreateTab("Moonrock")
+
+local ExampleSection = Library:CreateTabSection("Aimbot")
+ExampleSection.CreateSectionTab("Visuals")
+ExampleSection.CreateSectionTab("Trolling")
+ExampleSection.CreateSectionTab("Misc")
+ExampleSection.CreateSectionTab("Settings")
+
+local GroupBox = Tab:CreateGroupBox({Title = "Moonrock", Side = "Left"})
+
+GroupBox:CreateButton({
+    Title = "Hello, world!",
+    Callback = function()
+        print("hello world")
+    end
+})
